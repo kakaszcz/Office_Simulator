49075e14721289d8a7b962360ebab497cabcaf8b
Karolina Kaszczyszyn <karolinakaszczyszyn8@gmail.com>
2026-05-21 18:29:31 +0200

    poprawki konstruktorow

--- src/Senior.java
+++ src/Senior.java
@@ -1,4 +1,4 @@
-public class Senior extends Worker{
+public class Senior extends Worker {
     public Senior(int x, int y, double efficiency, double experience) {
 
         super(x, y, efficiency, experience);
