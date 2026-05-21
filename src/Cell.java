fd4ef71dab02b0f4c98a3cb6b882203260c630cb
Victoria01 <victoria.p26xv@gmail.com>
2026-05-21 17:01:03 +0200

    Zmiany

--- src/Cell.java
+++ src/Cell.java
@@ -1,8 +1,48 @@
 public class Cell {
     private int x;
     private int y;
-    private String type;
-    private Agent agent;
+    private String type;    //zmienna bedzie przyjmowała wartości: 'desk', 'coffee', 'outdoor', 'floor'
+    private Agent agent;    //referencja do agenta
 
+    //Konstruktor - wywoływany przez klasę board
+    public Cell(int x, int y, String type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.agent = null;      //ustawienie flagi początkowej na 0
+    }
+
+    //metoda połączona z  findFirstEmptyCell
+    //zwraca true jęsli kafelek(Cell) jest pusty (nikt na nim nie stoi)
+    public boolean isEmpty(){
+        return this.agent == null;
+    }
+
+    // Gettery i Settery
+    // Wyciągają wartości private HERMETYZACJA
+    public String getType(){    //
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public Agent getAgent(){
+        return agent;
+    }
+
+    //Ta metoda będzie przydzielała biurka i je zabierała
+    public void setAgent(Agent agent){
+        this.agent = agent;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
 
 }
---
+++ src/Worker.java
@@ -0,0 +1,6 @@
+public class Worker extends Agent {
+
+    public Worker(int x, int y, double wydajnosc, double doswiadczenie) {
+        super(x, y, wydajnosc, doswiadczenie);
+    }
+}
