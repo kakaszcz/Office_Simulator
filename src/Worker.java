195516c3c5733e2259d2c9ee6dff9c0b050347e3
Karolina Kaszczyszyn <karolinakaszczyszyn8@gmail.com>
2026-05-26 13:20:48 +0200

    Dodałam ENUM stanów pracownika, zmienilam metode shouldbefired na boolean oraz dodalam metode ktora tego booleana bedzie zmianiac na true abo false przez szefa. Dodalam mechanizm przydzielania taskow dla workera ktory bedzie wywolywany w symulacji co 3 tury (TO TRZEBA BEDZIE DODAC DO KLASY SIMULATION). Dodałam mechanizm obliczania task time. W workerze dodałam zmienne ktore pozniej beda potrzebne. ZMIENIALAM TYLKO KLASE WORKER

--- src/Worker.java
+++ src/Worker.java
@@ -2,9 +2,18 @@ public abstract class Worker extends Agent {
 
     private double efficiency;
     private double experience;
+    protected WorkerState state = WorkerState.WAITING;
+    private int taskTime = 0;
+    private int restTurns = 0;
+    private int cryingTurns = 0;
+    private int repairingTurns = 0;
+    private int talkingTurns = 0;
     private int roundsToFinishTask;
     private boolean isOutside;
-    private int taskTime;
+    private boolean atCoffeeTable = false;
+    private boolean shouldBeFired = false;
+    private boolean hasTask = false;
+    private int coffeesDrunk = 0;
 
     public Worker(int x, int y, double efficiency, double experience) {
         super(x, y);
@@ -18,11 +27,24 @@ public abstract class Worker extends Agent {
         return performance = (efficiency + experience)/2.0;
     }
 
-    public boolean shouldBeFired() { return false; }
+    //BEDZIE WYWOLYWANE PRZEZ KLASE SYMULACJA!!!!!!!! (co 3 tury lub co ile ustalimy ze jest przydielany task)
+    public void assignTask() {
+        if (state == WorkerState.WAITING) {
+            hasTask = true;
+        }
+    }
+
+    public boolean isShouldBeFired() { return shouldBeFired; }
+    public void markFired() {this.shouldBeFired = true; }
 
     public void doTask() {}
 
     public void goRest() {}
+
+//======== ile zajmie mu task(obliczanie)
+    protected int computeTaskTime() {
+        return Math.max(1, (int) Math.round(1 + 4.0 / (1.0 + getPerformance())));
+    }
 
     //public boolean isBossNear() {}
 }
